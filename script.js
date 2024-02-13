0096451c166997f5a1f8a4a4786723880ba0782e
varuncangit <159138260+varuncangit@users.noreply.github.com>
2024-02-13 22:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-// getting all required elements
 const inputBox = document.querySelector(".inputField input");
 const addButton = document.querySelector(".inputField button");
 const taskList = document.querySelector(".todoList");
-const deleteAllBtn = document.querySelector(".footer button");
+const deleteButton = document.querySelector(".footer button");
 
 inputBox.addEventListener('input', () => {
-    let userEnteredValue = inputBox.value; //getting user entered value
-    if (userEnteredValue.trim() != 0) { //if the user value isn't only spaces
-        addButton.classList.add("active"); //active the add button
+    let enterValue = inputBox.value;
+    if (enterValue.trim() != 0) {
+        addButton.classList.add("active");
     } else {
-        addButton.classList.remove("active"); //unactive the add button
+        addButton.classList.remove("active");
     }
 });
 
-showTasks(); //calling showTask function
+showTasks(); 
 
-addButton.onclick = () => { //when user click on plus icon button
-    let userEnteredValue = inputBox.value; //getting input field value
-    let getLocalStorageData = localStorage.getItem("TodoListItem"); //getting localstorage
-    if (getLocalStorageData == null) { //if localstorage has no data
-        listArray = []; //create a blank array
+addButton.onclick = () => { 
+    let enterValue = inputBox.value; 
+    let getLocalStorageData = localStorage.getItem("TodoListItem"); 
+    if (getLocalStorageData == null) { 
+        listArray = []; 
     } else {
-        listArray = JSON.parse(getLocalStorageData); //transforming json string into a js object
+        listArray = JSON.parse(getLocalStorageData); 
     }
-    listArray.push({task:userEnteredValue, checked:false}); //pushing or adding new value in array
-    localStorage.setItem("TodoListItem", JSON.stringify(listArray)); //transforming js object into a json string
-    showTasks(); //calling showTask function
-    addButton.classList.remove("active"); //unactive the add button once the task added
+    listArray.push({task:enterValue, checked:false}); 
+    localStorage.setItem("TodoListItem", JSON.stringify(listArray)); 
+    showTasks(); 
+    addButton.classList.remove("active"); 
 }
 
 function showTasks() {
@@ -37,21 +36,20 @@ function showTasks() {
         listArray = JSON.parse(getLocalStorageData);
     }
     const taskLeftNumb = document.querySelector(".taskLeft");
-    taskLeftNumb.textContent = listArray.length; //passing the array length in pendingtask
-    if (listArray.length > 0) { //if array length is greater than 0
-        deleteAllBtn.classList.add("active"); //active the delete button
+    taskLeftNumb.textContent = listArray.length; 
+    if (listArray.length > 0) { 
+        deleteButton.classList.add("active"); 
     } else {
-        deleteAllBtn.classList.remove("active"); //unactive the delete button
+        deleteButton.classList.remove("active");
     }
     let newLiTag = "";
     listArray.forEach((element, index) => {
         newLiTag += `<li><input type="checkbox" id="task-${index}" ${element.checked ? 'checked' : ''} onchange="toggleTask(${index})"><label for="task-${index}">${element.task}</label><span class="icon" onclick="deleteTask(${index})"><i class="fa-regular fa-circle-xmark"></i></span></li>`;
     });
-    taskList.innerHTML = newLiTag; //adding new li tag inside ul tag
-    inputBox.value = ""; //once task added leave the input field blank
+    taskList.innerHTML = newLiTag; 
+    inputBox.value = ""; 
 }
 
-// toggle task function
 function toggleTask(index) {
     let getLocalStorageData = localStorage.getItem("TodoListItem");
     listArray = JSON.parse(getLocalStorageData);
@@ -60,18 +58,16 @@ function toggleTask(index) {
     showTasks(); //call the showTasks function
 }
 
-// delete task function
 function deleteTask(index) {
     let getLocalStorageData = localStorage.getItem("TodoListItem");
     listArray = JSON.parse(getLocalStorageData);
-    listArray.splice(index, 1); //delete or remove the li
+    listArray.splice(index, 1); 
     localStorage.setItem("TodoListItem", JSON.stringify(listArray));
-    showTasks(); //call the showTasks function
+    showTasks(); 
 }
 
-// delete all tasks function
-deleteAllBtn.onclick = () => {
-    listArray = []; //empty the array
-    localStorage.setItem("TodoListItem", JSON.stringify(listArray)); //set the item in localstorage
-    showTasks(); //call the showTasks function
+deleteButton.onclick = () => {
+    listArray = []; 
+    localStorage.setItem("TodoListItem", JSON.stringify(listArray)); 
+    showTasks(); 
 }
